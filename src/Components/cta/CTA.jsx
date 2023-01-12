93164f201592f60f8bf58bd0c9961f32839963d8
Khushi <khushisingh4287702@gmail.com>
2023-01-13 02:00:06 +0530

    Ui-Ux design form figma

---
+++ src/Components/cta/CTA.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './CTA.css'
+
+const CTA = () => {
+  return (
+    <div className='gpt3__cta section__margin'>
+      <div className='gpt3_s_cta-text'>
+        <p>Request Early Access to Get Started</p>
+        <h2>Register today & start exploring the endless possiblities.</h2>
+      </div>
+      <div>
+        <button type='button' className='gpt3__cta-button'>Get Started</button>
+      </div>
+      
+    </div>
+  )
+}
+
+export default CTA
